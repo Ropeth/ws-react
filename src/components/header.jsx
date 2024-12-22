68e4351ce3b156e79f706c1a53afea5d9c40e8d6
Ropeth <121176515+Ropeth@users.noreply.github.com>
2024-12-22 13:21:09 +0000

    Bootstrap header added

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,12 +4,33 @@ import wsLogo from "../assets/logo.svg";
 export default function Header() {
   return (
     <div className="header">
-      <div className="nav">
+      {/* <div className="nav">
         <a href="/" target="_blank">
           <img src={wsLogo} className="logo" alt="Water Schools" />
         </a>
         <Menu />
+        <div className="tagline">
+        <p>A co-created community space for water regeneration</p>
       </div>
+      </div>
+       */}
+      <nav className="nav navbar navbar-expand-lg">
+        <a className="navbar-brand" href="#">
+          <img src={wsLogo} className="logo" alt="Water Schools" />
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNavDropdown"
+          aria-controls="navbarNavDropdown"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <Menu />
+      </nav>
       <div className="tagline">
         <p>A co-created community space for water regeneration</p>
       </div>
--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -1,21 +1,69 @@
 export default function Menu() {
   return (
-    <div className="menu">
-      <ul>
-        <li>
-          <a href="">Water School Africa</a>
+    // <div className="menu">
+    //   <ul>
+    //     <li>
+    //       <a href="">Water School Africa</a>
+    //     </li>
+    //     <li>
+    //       <a href="">Water School India</a>
+    //     </li>
+    //     <li>
+    //       <a href="">Water School Europe</a>
+    //     </li>
+    //     <li>
+    //       <a href="">Contact</a>
+    //     </li>
+    //     <li>
+    //       <a href="">More</a>
+    //     </li>
+    //   </ul>
+    // </div>
+    <div
+      className="menu collapse navbar-collapse justify-content-end"
+      id="navbarNavDropdown"
+    >
+      <ul className="navbar-nav">
+        <li className="nav-item active">
+          <a className="nav-link" href="#">
+            Water School Africa <span className="sr-only">(current)</span>
+          </a>
         </li>
-        <li>
-          <a href="">Water School India</a>
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            Water School India
+          </a>
         </li>
-        <li>
-          <a href="">Water School Europe</a>
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            Water School Europe
+          </a>
         </li>
-        <li>
-          <a href="">Contact</a>
-        </li>
-        <li>
-          <a href="">More</a>
+        <li className="nav-item dropdown">
+          <a
+            className="nav-link dropdown-toggle"
+            href="#"
+            id="navbarDropdownMenuLink"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
+            Contact
+          </a>
+          <div
+            className="dropdown-menu"
+            aria-labelledby="navbarDropdownMenuLink"
+          >
+            <a className="dropdown-item" href="#">
+              Action
+            </a>
+            <a className="dropdown-item" href="#">
+              Another action
+            </a>
+            <a className="dropdown-item" href="#">
+              Something else here
+            </a>
+          </div>
         </li>
       </ul>
     </div>
